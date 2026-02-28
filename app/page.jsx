b774eb2d03abc104d32e31f49216a827a1ea970a
bingos2 <inspiring.out@gmail.com>
2026-02-28 23:28:39 +0900

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{background:'#0D0A15', color:'white', height:'100vh', display:'flex', alignItems:'center', justifyContent:'center', fontSize:'32px'}}>
-      🔮 Nora is here
-    </div>
-  );
-}
