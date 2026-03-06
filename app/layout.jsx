bd1063ecd71e62e2d36456809b779adf240a6d4e
bingos2 <inspiring.out@gmail.com>
2026-03-06 23:39:07 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,21 +39,32 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-17991018308');
           `}
         </Script>
-<Script id="meta-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1444105087414207');
-    fbq('track', 'PageView');
-  `}
-</Script>
-
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4PJ35W6QR0"
+          strategy="afterInteractive"
+          />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4PJ35W6QR0');
+          `}
+        </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1444105087414207');
+          fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body>
         <noscript>
